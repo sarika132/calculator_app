86a44ce67f4f68092bcb1ebc2b5c8b536af6eb18
Sarika Pandey <sarika@Sarikas-MacBook-Pro.local>
2026-05-19 12:50:10 +0545

    first commit

---
+++ android/app/src/main/kotlin/com/sarika/calculator_app/calculator_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sarika.calculator_app.calculator_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:calculator_app/views/calculator_screen.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const CalculatorScreen(),
+    );
+  }
+}
